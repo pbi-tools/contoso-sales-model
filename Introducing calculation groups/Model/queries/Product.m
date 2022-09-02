6c34fa2b39c43d8cc71c4b78f180aa4692fa4b4c
Mathias Thierbach <mt-dev@outlook.com>
2022-09-02 16:26:30 +0100

    Converted all Web.Contents() queries to use RelativePath * Ensures only a single data source is detected in PBI Service * See: https://blog.crossjoin.co.uk/2016/08/16/using-the-relativepath-and-query-options-with-web-contents-in-power-query-and-power-bi-m-code/

--- Introducing calculation groups/Model/queries/Product.m
+++ Introducing calculation groups/Model/queries/Product.m
@@ -1,7 +1,52 @@
 let
-    Source = Web.Contents(SourceUrl & "Product.csv"),
+    Source =
+        Web.Contents(
+            SourceUrl,
+            [RelativePath = "Product.csv"]
+        ),
     Csv = Csv.Document(Source),
-    #"Promoted Headers" = Table.PromoteHeaders(Csv, [PromoteAllScalars=true]),
-    #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"ProductKey", Int64.Type}, {"Product Code", type text}, {"Product Name", type text}, {"Manufacturer", type text}, {"Brand", type text}, {"Subcategory", type text}, {"Category", type text}, {"Color", type text}})
+    #"Promoted Headers" =
+        Table.PromoteHeaders(
+            Csv,
+            [PromoteAllScalars = true]
+        ),
+    #"Changed Type" =
+        Table.TransformColumnTypes(
+            #"Promoted Headers",
+            {
+                {
+                    "ProductKey",
+                    Int64.Type
+                },
+                {
+                    "Product Code",
+                    type text
+                },
+                {
+                    "Product Name",
+                    type text
+                },
+                {
+                    "Manufacturer",
+                    type text
+                },
+                {
+                    "Brand",
+                    type text
+                },
+                {
+                    "Subcategory",
+                    type text
+                },
+                {
+                    "Category",
+                    type text
+                },
+                {
+                    "Color",
+                    type text
+                }
+            }
+        )
 in
     #"Changed Type"
--- Introducing calculation groups/Model/queries/Promotion.m
+++ Introducing calculation groups/Model/queries/Promotion.m
@@ -1,8 +1,60 @@
 let
-    Source = Web.Contents(SourceUrl & "Promotion.csv"),
+    Source =
+        Web.Contents(
+            SourceUrl,
+            [RelativePath = "Promotion.csv"]
+        ),
     Csv = Csv.Document(Source),
-    #"Promoted Headers" = Table.PromoteHeaders(Csv, [PromoteAllScalars=true]),
-    #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"PromotionKey", Int64.Type}, {"Promotion Code", type text}, {"Promotion", type text}, {"Discount", type number}, {"Promotion Type", type text}, {"Promotion Category", type text}}),
-    #"Parsed Date" = Table.TransformColumns(#"Changed Type",{{"Start Date", each Date.From(DateTimeZone.From(_)), type date}, {"End Date", each Date.From(DateTimeZone.From(_)), type date}})
+    #"Promoted Headers" =
+        Table.PromoteHeaders(
+            Csv,
+            [PromoteAllScalars = true]
+        ),
+    #"Changed Type" =
+        Table.TransformColumnTypes(
+            #"Promoted Headers",
+            {
+                {
+                    "PromotionKey",
+                    Int64.Type
+                },
+                {
+                    "Promotion Code",
+                    type text
+                },
+                {
+                    "Promotion",
+                    type text
+                },
+                {
+                    "Discount",
+                    type number
+                },
+                {
+                    "Promotion Type",
+                    type text
+                },
+                {
+                    "Promotion Category",
+                    type text
+                }
+            }
+        ),
+    #"Parsed Date" =
+        Table.TransformColumns(
+            #"Changed Type",
+            {
+                {
+                    "Start Date",
+                    each Date.From(DateTimeZone.From(_)),
+                    type date
+                },
+                {
+                    "End Date",
+                    each Date.From(DateTimeZone.From(_)),
+                    type date
+                }
+            }
+        )
 in
     #"Parsed Date"
